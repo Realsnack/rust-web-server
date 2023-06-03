19c5f59060e5a6798efb948b1b1b85ec03bf2484
Michal Svačina <msvacina@outlook.com>
2023-06-03 07:11:38 +0200

    update route handler

--- src/route_handler.rs
+++ src/route_handler.rs
@@ -2,7 +2,6 @@ pub fn handle_route(request_path: &str) -> String {
     println!("Request path: {}", request_path);
 
     if request_path.ends_with("/") {
-        println!("Index route");
         return handle_index_route();
     }
 
@@ -11,7 +10,6 @@ pub fn handle_route(request_path: &str) -> String {
 }
 
 fn handle_index_route() -> String {
-    let response = format!("HTTP/1.1 {}\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>Document</title></head><body><p>Hello Workd!</p></body></html>", 200);
-    println!("Index route response: {}", response);
+    let response = format!("HTTP/1.1 {}\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>Document</title></head><body><p>Hello World!</p></body></html>", 200);
     response
 }
